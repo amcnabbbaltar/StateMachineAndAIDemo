60dece38c9fc5ce24407d445c3c1ab3973daec4c
amcnabbbaltar <123915347+amcnabbbaltar@users.noreply.github.com>
2025-10-27 15:08:18 -0400

    Update

--- Assets/Scripts/State/ChaseState.cs
+++ Assets/Scripts/State/ChaseState.cs
@@ -1,16 +1,30 @@
+using UnityEngine;
+
 public class ChaseState : IState
 {
     private AIController aiController;
 
     public StateType Type => StateType.Chase;
+    
+    private Renderer aiRenderer;
+    private Color originalColor;
+    private Color chaseColor = new Color(1f, 0.6470588f, 0f, 1f); // Color when it sees the player
 
     public ChaseState(AIController aiController)
     {
         this.aiController = aiController;
+        aiRenderer = aiController.GetComponent<Renderer>();
+
+        if (aiRenderer != null)
+        {
+            originalColor = aiRenderer.material.color;
+        }
     }
 
     public void Enter()
     {
+           if (aiRenderer != null)
+            aiRenderer.material.color = chaseColor;
         // No animations, so no need to set any animator parameters
     }
 
@@ -33,6 +47,8 @@ public class ChaseState : IState
 
     public void Exit()
     {
+        if (aiRenderer != null)
+            aiRenderer.material.color = originalColor;
         // No cleanup necessary
     }
 }
